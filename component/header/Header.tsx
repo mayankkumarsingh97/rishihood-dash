3712a86b41c18a16804d2fdb27b160ef185b354c
Mayank kumar singh <mayank.reactjs@gmail.com>
2025-08-12 15:58:44 +0530

    add: Header with sorting dropdown

--- component/header/Header.tsx
+++ component/header/Header.tsx
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
+"use client";
+import React from "react";
 
 const Header = ({
-  onSearch,
   onDepartmentChange,
   onStatusChange,
 }: {
-  onSearch: (value: string) => void;
   onDepartmentChange: (value: string) => void;
   onStatusChange: (value: string) => void;
 }) => {
-  const [search, setSearch] = useState("");
-  const [isFocused, setIsFocused] = useState(false);
-
   const departments = [
     "All",
     "Computer Science",
@@ -20,32 +16,22 @@ const Header = ({
     "Chemistry",
     "Biology",
   ];
-  const statuses = ["All", "Active", "Inactive"];
+  const statuses = ["All", "active", "Inactive"];
 
   return (
     <div className="flex flex-wrap items-center gap-4 p-4 bg-white shadow-md rounded-lg mb-1">
-      {/* Search Input */}
-      <div className="relative">
-        <input
-          type="text"
-          value={search}
-          onChange={(e) => {
-            setSearch(e.target.value);
-            onSearch(e.target.value);
-          }}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          placeholder="Search by name ..."
-          className={`transition-all duration-300 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${
-            isFocused ? "w-92" : "w-48"
-          }`}
-        />
-      </div>
-
       {/* Department Dropdown */}
+      <label
+        htmlFor="department"
+        className="text-[14px] text-[#767676] font-roboto"
+      >
+        Sort by department:
+      </label>
       <select
+        name="department"
+        id="department"
         onChange={(e) => onDepartmentChange(e.target.value)}
-        className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full md:w-48 text-[14px] text-[#767676] font-roboto"
       >
         {departments.map((dept) => (
           <option key={dept} value={dept}>
@@ -55,9 +41,17 @@ const Header = ({
       </select>
 
       {/* Active Status Dropdown */}
+      <label
+        htmlFor="status"
+        className="text-[14px] text-[#767676] font-roboto"
+      >
+        Sort by status:
+      </label>
       <select
+        name="status"
+        id="status"
         onChange={(e) => onStatusChange(e.target.value)}
-        className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full md:w-48 text-[14px] text-[#767676] font-roboto"
       >
         {statuses.map((status) => (
           <option key={status} value={status}>

87dae04cf71eaeda17f0d6cc30427b7d9640820d
Mayank kumar singh <mayank.reactjs@gmail.com>
2025-08-12 15:55:35 +0530

    add: student data fetch Ctrl

---
+++ services/student.service.ts
@@ -0,0 +1,43 @@
+interface FetchStudentsParams {
+  page: number;
+  limit: number;
+  sortBy?: string;
+  order?: "asc" | "desc";
+  department?: string;
+  active?: string; // "true" | "false"
+  all:string
+}
+
+export const fetchStudents = async ({
+  page,
+  limit,
+  sortBy,
+  order,
+  department,
+  active,
+  all
+}: FetchStudentsParams) => {
+  try {
+    let query = `_page=${page}`;
+    if (sortBy) query += `&_sort=${sortBy}&_order=${order || "asc"}`;
+    if (department) query += `&department=${encodeURIComponent(department)}`;
+    if (active) query += `&active=${active}`;
+    if (all) query += `&all=${all}`;
+
+    const res = await fetch(`http://localhost:4000/students?${query}`);
+
+    if (!res.ok) throw new Error(`HTTP Error: ${res.status}`);
+
+    const total = res.headers.get("X-Total-Count");
+    const data = await res.json();
+
+    return { data, total: total ? Number(total) : 0 };
+  } catch (error) {
+    console.error("Error fetching students:", error);
+    return { data: [], total: 0 };
+  }
+};
+
+
+
+

611b9c1029ea06388137363aa7aed65e47dd4724
Mayank kumar singh <mayank.reactjs@gmail.com>
2025-08-13 12:05:36 +0530

    fix: next.config changed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
-};
 
+};
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: isProd ? '/rishihood-dash' : '',
+  assetPrefix: isProd ? '/rishihood-dash/' : '',
+};
 export default nextConfig;

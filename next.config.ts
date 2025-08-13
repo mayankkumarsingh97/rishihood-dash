1614620383a70258f30a5b01091d02b2ac440893
Mayank kumar singh <mayank.reactjs@gmail.com>
2025-08-13 11:51:07 +0530

    fix:next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export", // New way to enable static export
-  images: { unoptimized: true },
-  basePath: "/rishihood-dash",
-  assetPrefix: "/rishihood-dash/",
+  
 };
 
 export default nextConfig;
